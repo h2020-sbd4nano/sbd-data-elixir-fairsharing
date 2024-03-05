0ab5ad0c6d8ec59c44cd778bedc070b1233c7bd9
Egon Willighagen <egon.willighagen@gmail.com>
2024-03-05 16:03:20 +0100

    Use Bacting

--- convert.groovy
+++ convert.groovy
@@ -1,40 +1,56 @@
-// Copyright (c) 2022  Egon Willighagen <egon.willighagen@gmail.com>
+// Copyright (c) 2022-2024  Egon Willighagen <egon.willighagen@gmail.com>
 //
 // GPL v3
 
+@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.5.2')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.5.2')
+
 import groovy.json.JsonSlurper
+
+bioclipse = new net.bioclipse.managers.BioclipseManager(".");
+rdf = new net.bioclipse.managers.RDFManager(".");
 
 def jsonSlurper = new JsonSlurper()
 jsonContent = new File("toxicology.json").text
 records = jsonSlurper.parseText(jsonContent).data
 
-println "@prefix dc:    <http://purl.org/dc/elements/1.1/> ."
-println "@prefix dct:   <http://purl.org/dc/terms/> ."
-println "@prefix foaf:  <http://xmlns.com/foaf/0.1/> ."
-println "@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> ."
-println "@prefix sbd:   <https://www.sbd4nano.eu/rdf/#> ."
-println "@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> ."
-println "@prefix void:  <http://rdfs.org/ns/void#> ."
-println ""
-println "<https://fairsharing.org/>"
-println " a                    void:DatasetDescription ;"
-println " dc:source            <https://fairsharing.org/> ;"
-println " dct:title            \"FAIRsharing\"@en ;"
-println " dct:license          <http://creativecommons.org/licenses/by-sa/4.0/> ;"
-println " foaf:img             <https://api.fairsharing.org/img/fairsharing-attribution.svg> ."
-println ""
+store = rdf.createInMemoryStore()
+
+rdf.addPrefix(store, "dc", "http://purl.org/dc/elements/1.1/")
+rdf.addPrefix(store, "dct", "http://purl.org/dc/terms/")
+rdf.addPrefix(store, "foaf", "http://xmlns.com/foaf/0.1/")
+rdf.addPrefix(store, "rdfs", "http://www.w3.org/2000/01/rdf-schema#")
+rdf.addPrefix(store, "sbd", "https://www.sbd4nano.eu/rdf/#")
+rdf.addPrefix(store, "xsd", "http://www.w3.org/2001/XMLSchema#")
+rdf.addPrefix(store, "void", "http://rdfs.org/ns/void#")
+
+propType    = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+propLabel   = "http://www.w3.org/2000/01/rdf-schema#label"
+propSource  = "http://purl.org/dc/elements/1.1/source"
+propDesc    = "http://purl.org/dc/elements/1.1/description" 
+propLicense = "http://purl.org/dc/terms/license"
+propFOAFpage= "http://xmlns.com/foaf/0.1/page"
+
+metaIRI = "https://fairsharing.org/"
+rdf.addObjectProperty(store, metaIRI, propType, "http://rdfs.org/ns/void#DatasetDescription")
+rdf.addObjectProperty(store, metaIRI, propSource, "https://fairsharing.org/")
+rdf.addPropertyInLanguage(store, metaIRI, "http://purl.org/dc/terms/title", "FAIRsharing", "en")
+rdf.addObjectProperty(store, metaIRI, "http://xmlns.com/foaf/0.1/img", "https://api.fairsharing.org/img/fairsharing-attribution.svg")
+rdf.addObjectProperty(store, metaIRI, propLicense, "http://creativecommons.org/licenses/by-sa/4.0/")
 
 for (record in records) {
-  println "<${record.attributes.url}> a sbd:Database ;"
+  recordIRI = record.attributes.url
+  rdf.addObjectProperty(store, recordIRI, propType, "https://www.sbd4nano.eu/rdf/#Database")
   if (record.attributes.metadata.name != null)
-    println "  rdfs:label \"${record.attributes.metadata.name}\"@en ;"
+    rdf.addPropertyInLanguage(store, recordIRI, propLabel, record.attributes.metadata.name, "en")
   if (record.attributes.metadata.description != null)
-    println "  dc:description \"${record.attributes.metadata.description}\"@en ;"
+    rdf.addPropertyInLanguage(store, recordIRI, propDesc, record.attributes.metadata.description, "en")
   if (record.attributes.metadata.homepage != null)
-    println "  foaf:page \"${record.attributes.metadata.homepage}\"@en ;"
+    rdf.addObjectProperty(store, recordIRI, propFOAFpage, record.attributes.metadata.homepage)
   if (record.attributes.licence_links != null && record.attributes.licence_links[0] != null) {
-    println "  dct:license <${record.attributes.licence_links[0].licence_url}> ;"
+    rdf.addObjectProperty(store, recordIRI, propLicense, record.attributes.licence_links[0].licence_url)
   }
-  println "  dc:source <https://fairsharing.org/> ."
-  println ""
+  rdf.addObjectProperty(store, recordIRI, propSource, "https://fairsharing.org/")
 }
+
+println rdf.asTurtle(store)
